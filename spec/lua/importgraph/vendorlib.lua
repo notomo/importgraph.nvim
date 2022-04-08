a356ba6f02ce40cf0edde9a9dfec4f5bdc12bc69
notomo <notomo.motono@gmail.com>
2022-04-08 09:41:40 +0900

    Update vendorlib

--- spec/lua/importgraph/vendorlib.lua
+++ spec/lua/importgraph/vendorlib.lua
@@ -1,6 +1,6 @@
 return {
-  "lua/misclib/error_handler.lua",
-  "lua/misclib/message.lua",
-  "lua/misclib/module.lua",
-  "lua/misclib/collection/ordered_dict.lua",
+  "notomo/misclib.nvim/lua/misclib/error_handler.lua",
+  "notomo/misclib.nvim/lua/misclib/message.lua",
+  "notomo/misclib.nvim/lua/misclib/module.lua",
+  "notomo/misclib.nvim/lua/misclib/collection/ordered_dict.lua",
 }
