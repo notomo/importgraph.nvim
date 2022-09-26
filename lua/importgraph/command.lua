3ae96167597af746f2df89a1ca682b15aa4badbb
notomo <notomo.motono@gmail.com>
2022-09-26 11:38:52 +0000

    Format (stylua)

--- lua/importgraph/command.lua
+++ lua/importgraph/command.lua
@@ -3,18 +3,14 @@ local ReturnValue = require("importgraph.vendor.misclib.error_handler").for_retu
 function ReturnValue.render(raw_opts)
   local opts = require("importgraph.core.option").new(raw_opts)
 
-  local paths, collect_err = require("importgraph.core.file_path").collect(
-    opts.collector.working_dir,
-    opts.collector.path_filter
-  )
+  local paths, collect_err =
+    require("importgraph.core.file_path").collect(opts.collector.working_dir, opts.collector.path_filter)
   if collect_err then
     return nil, collect_err
   end
 
-  local graph_factory, factory_err = require("importgraph.core.graph_factory").new(
-    opts.collector.working_dir,
-    opts.collector.language
-  )
+  local graph_factory, factory_err =
+    require("importgraph.core.graph_factory").new(opts.collector.working_dir, opts.collector.language)
   if factory_err then
     return nil, factory_err
   end
