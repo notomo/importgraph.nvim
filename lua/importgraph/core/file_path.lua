0f11c06d83e3436944206393f322effe7cd563a2
notomo <notomo.motono@gmail.com>
2022-12-29 19:47:56 +0900

    Adjust file path collector

--- lua/importgraph/core/file_path.lua
+++ lua/importgraph/core/file_path.lua
@@ -1,10 +1,13 @@
+local fn = vim.fn
+
 local M = {}
 
 function M.collect(working_dir, path_filter)
-  local paths = vim.fn.glob(working_dir .. "**", false, true)
+  local paths = fn.glob(working_dir .. "**", false, true)
 
   return vim.tbl_filter(function(path)
-    return vim.fn.isdirectory(path) == 0 and path_filter(path)
+    path = fn.fnamemodify(path, ":p")
+    return fn.isdirectory(path) == 0 and path_filter(path)
   end, paths)
 end
 
