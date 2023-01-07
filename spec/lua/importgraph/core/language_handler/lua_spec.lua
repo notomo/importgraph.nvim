a28ef00b469798a03fbeaf786fdcad1dc45b1295
notomo <notomo.motono@gmail.com>
2023-01-07 15:33:35 +0900

    Move lua handler test

--- spec/lua/importgraph/core/language_handler/lua_spec.lua
+++ spec/lua/importgraph/core/language_handler/lua_spec.lua
@@ -1,17 +1,24 @@
 local helper = require("importgraph.test.helper")
 local importgraph = helper.require("importgraph")
 
-describe("render()", function()
+describe("render() with lua", function()
   before_each(helper.before_each)
   after_each(helper.after_each)
 
-  it("returns graph including orphan node", function()
+  it("returns graph text", function()
     helper.install_parser("lua")
 
     helper.test_data:create_file(
       "node1.lua",
       [[
-return "test"
+require("node2")
+return require("node3")
+]]
+    )
+    helper.test_data:create_file(
+      "node2.lua",
+      [[
+return require("other")
 ]]
     )
 
@@ -21,18 +28,10 @@ return "test"
     assert.equal(
       [[
 graph TB
-  1(node1)]],
+  1(node1) --> 2(node2)
+  1(node1) --> 3(node3)
+  2(node2) --> 4(other)]],
       graph
     )
-  end)
-
-  it("raises error if renderer is not found", function()
-    local ok, got = pcall(function()
-      importgraph.render({
-        renderer = { name = "invalid" },
-      })
-    end)
-    assert.is_false(ok)
-    assert.equals([=[[importgraph] not found renderer: invalid]=], got)
   end)
 end)
