28e0d2d50876e7ae29e90ac84eec81180e61a648
notomo <notomo.motono@gmail.com>
2023-02-19 19:19:11 +0900

    Add option doc

--- lua/importgraph/init.lua
+++ lua/importgraph/init.lua
@@ -1,8 +1,21 @@
 local M = {}
 
+--- @class Option
+--- @field renderer RendererOption?: |importgraph.RendererOption|
+--- @field collector CollectorOption?: |importgraph.CollectorOption|
+
+--- @class RendererOption
+--- @field name string? (default: "mermaid")
+--- @field opts table? renderer specific option
+
+--- @class CollectorOption
+--- @field working_dir string? (default: ".")
+--- @field path_filter (fun(path:string):boolean)?
+--- @field imported_target_filter (fun(imported_target:string):boolean)?
+
 --- Returns import graph string
 --- @param language string: language name
---- @param opts table|nil: TODO
+--- @param opts Option? |importgraph.Option|
 --- @return string: import graph
 function M.render(language, opts)
   local rendered, err = require("importgraph.command").render(language, opts)
